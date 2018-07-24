75e76ee26fc29490a8905fcab712d2fad39e5ade
GalileoCappella <galileofcap@gmail.com>
2018-07-24 16:20:43 -0300

    Arreglamos el grafico: Corregimos la funcion principal, y eliminamos calculos innecesarios

--- app.R
+++ app.R
@@ -1,4 +1,15 @@
+# INFO: Simular un plano inclinado
+# El angulo del plano con la horizontal es alfa [grados]
+# La aceleracion de la gravedad es g [m/s^2]
+# La aceleracion en la direccion del plano es ag = g * sin(alfa)
+# La velocidad en el momento t es v(t) = v0 + ag * t
+# La posicion en el momento t es X(t) = X0 + g * sin(alfa) * 1/2 * t^2
+# Porqe a = dv/dt = d(dx/dt)/dt
+# O sea la aceleracion es la derivada de la velocidad, qe es la derivada de la posicion
+
 library(shiny)
+
+g <- 9.80665 #Constante de la gravedad
 
 ui <- fluidPage(
    
@@ -6,18 +17,13 @@ ui <- fluidPage(
    
    sidebarLayout(
       sidebarPanel(
-         sliderInput("ang",
-                     "Ángulo de caída:",
+         sliderInput("alfa",
+                     "Ángulo de inclinacion del plano:",
                      min = 1,
                      max = 90,
                      value = 30),
-         sliderInput("kg",
-                     "Peso:",
-                     min = 1,
-                     max = 100,
-                     value = 20),
-         sliderInput("dist",
-                     "Distancia de caída",
+         sliderInput("larg",
+                     "Largo de la caída",
                      min = 1,
                      max = 100,
                      value = 50)
@@ -32,24 +38,17 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      g <- 9.80665 #Constante de la gravedad
-      fg <- input$kg*g #Fuerza ejercida en la bola por la gravedad
-
-      angrad <- input$ang*(pi/180)  #Paso los grados qe la usuario me pidió, a radianes
+     
+      alfarad <- input$alfa*(pi/180)  #Paso los grados qe la usuario me pidió, a radianes
+      ag <- g*sin(alfarad)
+      tcuad <- (input$dist*2)/ag #Calculo el tiempo qe tarda en llegar al piso (separé el raizarlo por si no anda bien)
+      t <- tcuad^0.5 #Hago la raiz para obtener el tiempo
       
-      fc <- sin(angrad)*fg #Calculo la fuerza de la caída
-      a <- fc/input$kg #Calculo la aceleración de la caída
-      tcuad <- (input$dist*2)/a #Calculo el tiempo qe tarda en llegar al piso (separé el raizarlo por si no anda bien)
-      t <- tcuad^0.5 #Finalizo el cálculo del tiempo
-      
-      b <- sin(angrad)*input$dist #Calculo la altura inicial
-      cayo <- cos(angrad)*input$dist #Calculo la base sobre la qe está puesta la rampa
-
-      curve(-a*x+b,
-            xlim = c(0, t+1),
-            ylim = c(0, b),
+      curve(g*sin(alfarad)*0.5*x^2,
+            xlim = c(0, t+1), #Sumo 1segundo para qe el grafico qede mas lindo
+            ylim = c(0, input$larg*1.1), #Agrego un 10% extra para qe el grafico qede mas lindo
             xlab = "Tiempo de caida en segundos",
-            ylab = "Altura en metros",
+            ylab = "Posicion sobre el plano",
             col = 'darkgray', 
             )
    })
